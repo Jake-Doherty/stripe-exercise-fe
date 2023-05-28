3256c2b0d671c04ce2d72bf3d8db74f047df7cc7
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-05-28 14:59:20 -0700

    adds routes to app for ✔ and ❌

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
 import './App.css';
 import Button from './components/Button/Button.js';
 
 function App() {
   return (
     <div className="App">
-      <Button />
+      <Routes>
+        <Route path="/" element={<Button />} />
+        <Route path="/success" element={<h1>Success!</h1>} />
+        <Route path="/cancel" element={<h1>Cancel!</h1>} />
+      </Routes>
     </div>
   );
 }
