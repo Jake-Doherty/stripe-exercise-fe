af436d971ad854faaa0da961d8791d87b7727447
kevinnail <fumalicious@gmail.com>
2023-05-30 19:49:13 -0700

    removed body from req for subs purchase (works)

--- src/services/stripe.js
+++ src/services/stripe.js
@@ -5,12 +5,12 @@ export async function fetchStripe() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        items: [
-          { id: 1, quantity: 3 },
-          { id: 2, quantity: 3 },
-        ],
-      }),
+      // body: JSON.stringify({
+      //   items: [
+      //     { id: 1, quantity: 3 },
+      //     { id: 2, quantity: 3 },
+      //   ],
+      // }),
     });
     const data = await resp.json();
 
