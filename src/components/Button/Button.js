c59d115cf16257a81dddb5a55c86c2f0a6af8df4
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-06-02 16:01:46 -0700

    adds second button and priceId to value

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,10 +2,28 @@ import React from 'react';
 import { fetchStripe } from '../../services/stripe.js';
 
 export default function Button() {
-  const handleClick = async () => {
-    const data = await fetchStripe();
+  const handleClick = async (e) => {
+    const priceId = e.target.value;
+    const data = await fetchStripe({ priceId });
     return data;
   };
 
-  return <button onClick={handleClick}>Monthly</button>;
+  return (
+    <>
+      <button
+        style={{ margin: '10px' }}
+        value={'price_1NCti2BhW6CkNmXjGPYwdTx6'}
+        onClick={(e) => handleClick(e)}
+      >
+        Monthly
+      </button>
+      <button
+        style={{ margin: '10px' }}
+        value={'price_1NCtiXBhW6CkNmXjtbi0h2Om'}
+        onClick={(e) => handleClick(e)}
+      >
+        Annual
+      </button>
+    </>
+  );
 }
