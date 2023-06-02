1e6805c9f5fe4dde53a7bee9d1cffb580f73edd1
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-06-02 16:02:27 -0700

    adds priceId to fetch call for backend stripe

--- src/services/stripe.js
+++ src/services/stripe.js
@@ -1,16 +1,11 @@
-export async function fetchStripe() {
+export async function fetchStripe({ priceId }) {
   try {
     const resp = await fetch('http://localhost:7890/api/v1/create-checkout-session', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      // body: JSON.stringify({
-      //   items: [
-      //     { id: 1, quantity: 3 },
-      //     { id: 2, quantity: 3 },
-      //   ],
-      // }),
+      body: JSON.stringify({ priceId }),
     });
     const data = await resp.json();
 
