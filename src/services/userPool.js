a6a041e847dcc38da48f28df59112732fb2e01c7
kevinnail <fumalicious@gmail.com>
2023-06-06 11:34:04 -0700

    clean up npm stuff- server working

--- src/services/userPool.js
+++ src/services/userPool.js
@@ -1,6 +1,6 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 // import 'dotenv/config ';
-require('dotenv').config();
+// require('dotenv').config();
 const poolData = {
   UserPoolId: process.env.REACT_APP_POOL_ID,
   ClientId: process.env.REACT_APP_APP_CLIENT_ID,
