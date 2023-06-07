45569bb8fcc8e362c6707e9aa861478760de8688
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-06-07 14:03:27 -0700

    adds conditional to dictate which fetch to use for sign in and sign up

--- src/Context/AccountContext.js
+++ src/Context/AccountContext.js
@@ -2,49 +2,47 @@ import { createContext, useContext } from 'react';
 const AccountContext = createContext();
 
 const AccountProvider = ({ children }) => {
-  async function fetchSignUp({ email, password }) {
-    try {
-      const resp = await fetch('http://localhost:7890/api/v1/auth/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-      const data = await resp.json();
-      if (resp.ok) {
-        return data;
-      } else {
-        return Promise.reject(data);
+  const fetchAuth = async ({ email, password, type }) => {
+    if (type === 'sign-up') {
+      try {
+        const resp = await fetch('http://localhost:7890/api/v1/auth/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        });
+        const data = await resp.json();
+        if (resp.ok) {
+          return data;
+        } else {
+          return Promise.reject(data);
+        }
+      } catch (e) {
+        console.error(e);
       }
-    } catch (e) {
-      console.error(e);
     }
-  }
-  async function fetchSignIn({ email, password }) {
-    try {
-      const resp = await fetch('http://localhost:7890/api/v1/auth/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-      const data = await resp.json();
-      if (resp.ok) {
-        return data;
-      } else {
-        return Promise.reject(data);
+    if (type === 'sign-in') {
+      try {
+        const resp = await fetch('http://localhost:7890/api/v1/auth/signin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        });
+        const data = await resp.json();
+        if (resp.ok) {
+          return data;
+        } else {
+          return Promise.reject(data);
+        }
+      } catch (e) {
+        console.error(e);
       }
-    } catch (e) {
-      console.error(e);
     }
-  }
-  return (
-    <AccountContext.Provider value={{ fetchSignUp, fetchSignIn }}>
-      {children}
-    </AccountContext.Provider>
-  );
+  };
+  return <AccountContext.Provider value={{ fetchAuth }}>{children}</AccountContext.Provider>;
 };
 
 const useAccount = () => {
