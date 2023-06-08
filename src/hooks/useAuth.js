f0c6fd09b399d46e64427c9e9096d4ea50b7867c
kevinnail <fumalicious@gmail.com>
2023-06-08 14:39:46 -0700

    added hook for sign up/ in/ out

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,65 @@
+export default function useAuth() {
+  const fetchAuth = async ({ email, password, type }) => {
+    if (type === 'sign-up') {
+      try {
+        const resp = await fetch('http://localhost:7890/api/v1/auth/sign-up', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        });
+        const data = await resp.json();
+        if (resp.ok) {
+          window.location = '/auth/sign-in';
+          return data;
+        } else {
+          return Promise.reject(data);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    if (type === 'sign-in') {
+      try {
+        const resp = await fetch('http://localhost:7890/api/v1/auth/sign-in', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+          credentials: 'include',
+        });
+        const data = await resp.json();
+        if (resp.ok) {
+          window.location = '/';
+          return data;
+        } else {
+          return Promise.reject(data);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  };
+
+  const handleSignOut = async (email_1) => {
+    try {
+      const resp = await fetch('http://localhost:7890/api/v1/auth/sign-out', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({ email_1 }),
+      });
+      const data = await resp.json();
+      if (resp.ok) {
+        window.location = '/auth/sign-in';
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return { fetchAuth, handleSignOut };
+}
