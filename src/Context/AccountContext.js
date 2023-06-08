f946573404a77dff36499eeb5596ebed569031d2
kevinnail <fumalicious@gmail.com>
2023-06-08 14:37:21 -0700

    adding sign out function and refactor into hooks

--- src/Context/AccountContext.js
+++ src/Context/AccountContext.js
@@ -1,49 +1,14 @@
 import { createContext, useContext } from 'react';
+import useAuth from '../hooks/useAuth.js';
 const AccountContext = createContext();
 
 const AccountProvider = ({ children }) => {
-  const fetchAuth = async ({ email, password, type }) => {
-    if (type === 'sign-up') {
-      try {
-        const resp = await fetch('http://localhost:7890/api/v1/auth/signup', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email, password }),
-        });
-        const data = await resp.json();
-        if (resp.ok) {
-          return data;
-        } else {
-          return Promise.reject(data);
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    }
-    if (type === 'sign-in') {
-      try {
-        const resp = await fetch('http://localhost:7890/api/v1/auth/signin', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email, password }),
-          credentials: 'include',
-        });
-        const data = await resp.json();
-        if (resp.ok) {
-          return data;
-        } else {
-          return Promise.reject(data);
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  };
-  return <AccountContext.Provider value={{ fetchAuth }}>{children}</AccountContext.Provider>;
+  const { fetchAuth, handleSignOut } = useAuth();
+  return (
+    <AccountContext.Provider value={{ fetchAuth, handleSignOut }}>
+      {children}
+    </AccountContext.Provider>
+  );
 };
 
 const useAccount = () => {
